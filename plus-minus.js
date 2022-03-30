5f839f87aed33ea29be36eed0e8dcc204377be61
Slicktone <tclaytony23@gmail.com>
2022-03-30 14:13:31 -0700

    print results of each

--- plus-minus.js
+++ plus-minus.js
@@ -35,6 +35,10 @@ function plusMinus(arr) {
         arr.array.forEach(i => {
             i > 0 ? pos++ : i < 0 ? neg++ : zero++;
         });
+        //print results of numbers positive, negative, and zero
+        console.log((pos/n).toFixed(6));
+        console.log((neg/n).toFixed(6));
+        console.log((zero/n).toFixed(6));
     }
 
 }
