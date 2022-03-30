269dd2d12f58f97d1b9dcb8b906ec69ce321e757
Slicktone <tclaytony23@gmail.com>
2022-03-30 14:20:19 -0700

    arr.forEach typo

--- plus-minus.js
+++ plus-minus.js
@@ -32,7 +32,7 @@ function plusMinus(arr) {
     let [pos, neg, zero] = [0, 0, 0];
     //open iteration
     if (n !== 0) {
-        arr.array.forEach(i => {
+        arr.forEach(i => {
             i > 0 ? pos++ : i < 0 ? neg++ : zero++;
         });
         //print results of numbers positive, negative, and zero
