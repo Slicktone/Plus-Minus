fb52515452068122f5464c63064aacf50c6c686d
Slicktone <tclaytony23@gmail.com>
2022-03-30 15:14:37 -0700

    readme update and cleanup

--- plus-minus.js
+++ plus-minus.js
@@ -1,31 +1,3 @@
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-/*
- * Complete the 'plusMinus' function below.
- *
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
-
 function plusMinus(arr) {
     //defining variables
     const n = arr.length;
@@ -41,12 +13,4 @@ function plusMinus(arr) {
         console.log((zero/n).toFixed(6));
     }
 
-}
-
-function main() {
-    const n = parseInt(readLine().trim(), 10);
-
-    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
-
-    plusMinus(arr);
 }
